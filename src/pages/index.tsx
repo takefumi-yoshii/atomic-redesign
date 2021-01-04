1633a5171371390b37782f253bc49e605eeabb94
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-04 15:47:41 +0900

    feat(page): use Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,8 @@
-const Page = () => <h1>Atomic ReDesign</h1>;
+import { Button } from "../components/atoms/Button";
+const Page = () => (
+  <div>
+    <h1>Atomic ReDesign</h1>
+    <Button>click me</Button>
+  </div>
+);
 export default Page;
