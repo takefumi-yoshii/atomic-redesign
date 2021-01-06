b6c9a65a9e6222f0afde33bbf9cfa2e14453838c
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-06 22:15:53 +0900

    feat(component): add templates Top

---
+++ src/components/templates/Top/Top.stories.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { TemplateTop } from "./Top";
+
+storiesOf("templates/TemplateTop", module).add("default", () => (
+  <TemplateTop />
+));
---
+++ src/components/templates/Top/Top.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "../../atoms/Layout";
+import styles from "./styles.module.css";
+
+const TemplateTop = () => (
+  <Layout>
+    <h1 className={styles.title}>Atomic ReDesign</h1>
+  </Layout>
+);
+
+export { TemplateTop };
