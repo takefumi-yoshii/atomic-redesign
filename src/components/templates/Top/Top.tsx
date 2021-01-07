fbb066b8c2d0b96ac6d9cb1f100333858a141e48
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-07 23:48:24 +0900

    feat(component): add card items

--- src/components/templates/Top/Top.tsx
+++ src/components/templates/Top/Top.tsx
@@ -1,9 +1,18 @@
 import { Layout } from "../../atoms/Layout";
+import { ThumbnailCard } from "../../molecules/ThumbnailCard";
 import styles from "./styles.module.css";
+import { data } from "./stub";
 
 const TemplateTop = () => (
   <Layout>
     <h1 className={styles.title}>Atomic ReDesign</h1>
+    <ul className={styles.list}>
+      {data.map((item) => (
+        <li className={styles.item}>
+          <ThumbnailCard title={item.title} body={item.body} img={item.img} />
+        </li>
+      ))}
+    </ul>
   </Layout>
 );
 
