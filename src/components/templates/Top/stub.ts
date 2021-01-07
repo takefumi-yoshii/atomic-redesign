8969e0116dea304823ad7194cca5931b1e0d756c
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-07 23:47:34 +0900

    feat(component): add stub

---
+++ src/components/templates/Top/stub.ts
@@ -0,0 +1,52 @@
+import type { Props as ThumbnailCardProps } from "../../molecules/ThumbnailCard";
+
+export const data: ThumbnailCardProps[] = [
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+  {
+    title: "Train",
+    body:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, vitae deleniti? Labore eligendi laudantium quae, ducimus omnis accusamus, ullam velit qui minima, vel asperiores maiores laborum eaque repellat doloremque esse.",
+    img: { src: "/images/wintry-3758523_640.jpg" },
+  },
+];
