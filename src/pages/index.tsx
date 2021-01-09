5d5b74bccccab9eb2ce2effb1551328bee8a7b70
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-09 09:57:28 +0900

    fix(page): add base components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import { TemplateTop } from "../components/templates/Top";
+import { Layout } from "../components/atoms/Layout";
+import { Footer } from "../components/atoms/Footer";
+import { Navigation } from "../components/organisms/Navigation";
 
-const Page = () => <TemplateTop />;
+const Page = () => (
+  <Layout>
+    <TemplateTop />
+    <Footer>
+      <Navigation />
+    </Footer>
+  </Layout>
+);
 
 export default Page;
