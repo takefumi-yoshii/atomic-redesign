f21ccd8b1af4cbd0cab6f16e0464fc88b78865a0
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-11 10:22:33 +0900

    fix(page): use template

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,11 @@
 import { Footer } from "../components/atoms/Footer";
 import { Layout } from "../components/organisms/Layout";
 import { Navigation } from "../components/organisms/Navigation";
+import { About } from "../components/templates/About";
 
 const Page = () => (
   <Layout>
-    about
+    <About />
     <Footer>
       <Navigation />
     </Footer>
--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,10 +1,11 @@
 import { Footer } from "../components/atoms/Footer";
 import { Layout } from "../components/organisms/Layout";
 import { Navigation } from "../components/organisms/Navigation";
+import { Example } from "../components/templates/Example";
 
 const Page = () => (
   <Layout>
-    example
+    <Example />
     <Footer>
       <Navigation />
     </Footer>
