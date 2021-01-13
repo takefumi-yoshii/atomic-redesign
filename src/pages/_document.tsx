c299293fa385582dc7f6be65d6827d9a3ea9342a
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-14 00:52:05 +0900

    chore(document): remove link font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,11 +14,6 @@ class MyDocument extends Document {
             name="description"
             content="'Atomic ReDesign' is an extension of the famous 'Atomic Design'. When designing components such as React and Vue.js, We often had trouble. UI granularity classification constraints can prevent component design optimization."
           />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap"
-            rel="stylesheet"
-          ></link>
         </Head>
         <body>
           <Main />
