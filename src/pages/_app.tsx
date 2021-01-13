da3322666cb2720618dcaa13533534c1bb75aa66
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-14 00:22:27 +0900

    refactor(app): move Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,5 @@
 import "../styles/reset.css";
-import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
