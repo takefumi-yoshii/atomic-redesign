dd17475e53b2e02a91e9ea3faeb65dd32fc56e7b
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-14 00:23:18 +0900

    chore(component): fix tag

--- src/components/templates/Top/Hero/Hero.tsx
+++ src/components/templates/Top/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 
 const Hero = () => (
   <div className={styles.module}>
-    <h1 className={styles.title}>Atomic ReDesign</h1>
+    <h2 className={styles.title}>Atomic ReDesign</h2>
   </div>
 );
 
