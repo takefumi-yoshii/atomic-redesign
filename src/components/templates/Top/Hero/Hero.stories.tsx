1e60d5bd8889e2760b143f7b9553db844596d0a6
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-13 10:21:26 +0900

    feat(component): add Hero

---
+++ src/components/templates/Top/Hero/Hero.stories.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import { Hero } from "./Hero";
+
+storiesOf("templates/Hero", module).add("default", () => <Hero />);
---
+++ src/components/templates/Top/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import styles from "./styles.module.css";
+
+const Hero = () => (
+  <div className={styles.module}>
+    <h1 className={styles.title}>Atomic ReDesign</h1>
+  </div>
+);
+
+export { Hero };
