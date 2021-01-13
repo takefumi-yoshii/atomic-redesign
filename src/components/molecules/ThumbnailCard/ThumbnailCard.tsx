4d730e0a6812a8eb3312c741f55c9cd680f4c7e3
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-14 00:51:33 +0900

    refactor(component): use next/image

--- src/components/molecules/ThumbnailCard/ThumbnailCard.tsx
+++ src/components/molecules/ThumbnailCard/ThumbnailCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import * as React from "react";
 import styles from "./styles.module.css";
 
@@ -12,10 +13,12 @@ export interface Props {
 
 const ThumbnailCard = (props: Props) => (
   <div className={styles.module}>
-    <p
-      className={styles.img}
-      style={{ backgroundImage: `url(${props.img.src})` }}
-    ></p>
+    <Image
+      src={props.img.src}
+      alt="Picture of the author"
+      width={640}
+      height={427}
+    />
     <div className={styles.outline}>
       <h3 className={styles.title}>{props.title}</h3>
       <p className={styles.body}>{props.body}</p>
--- src/components/templates/Top/Hero/Hero.tsx
+++ src/components/templates/Top/Hero/Hero.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
 import * as React from "react";
 import styles from "./styles.module.css";
 
 const Hero = () => (
   <div className={styles.module}>
+    <Image
+      src="/images/nature-3091991_1920.jpg"
+      alt="Picture of the author"
+      width={1920}
+      height={1080}
+    />
     <h2 className={styles.title}>Atomic ReDesign</h2>
   </div>
 );
