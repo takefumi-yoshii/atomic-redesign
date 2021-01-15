a3f9dec00cda69a2cd04143bb781766f723dc871
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-15 16:29:58 +0900

    refactor(component): add section

--- src/components/templates/Top/Top.tsx
+++ src/components/templates/Top/Top.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
 import { Cards } from "./Cards";
 import { Hero } from "./Hero";
+import { Lead } from "./Lead";
+import styles from "./styles.module.css";
 
 export const Top = () => (
-  <main>
-    <Hero />
-    <Cards />
+  <main className={styles.module}>
+    <section className={styles.hero}>
+      <Hero />
+    </section>
+    <h2 className={styles.title}>Atomic ReDesign</h2>
+    <section className={styles.lead}>
+      <Lead />
+    </section>
+    <section className={styles.cards}>
+      <Cards />
+    </section>
   </main>
 );
