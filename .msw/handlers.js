0ee8f3b28c70d7b91997a936af7e5e84f0cfbf23
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-18 02:21:50 +0900

    feat(msw): add msw entry

---
+++ .msw/handlers.js
@@ -0,0 +1,4 @@
+export const handlers = [
+  require("../src/components/templates/About/About.stub.ts").default,
+  require("../src/components/templates/Example/Example.stub.ts").default,
+].flat();
