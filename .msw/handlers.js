7d1dd15609f6618435221451014e72a7d23d519e
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2021-01-18 09:38:42 +0900

    chore(msw): remove path

--- .msw/handlers.js
+++ .msw/handlers.js
@@ -1,4 +1,3 @@
 export const handlers = [
-  require("../src/components/templates/About/About.stub.ts").default,
   require("../src/components/templates/Example/Example.stub.ts").default,
 ].flat();
